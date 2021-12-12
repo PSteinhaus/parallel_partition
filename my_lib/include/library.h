094df09454c1d754815650a099529c0a745bd9dc
PSteinhaus <philipp_steinhaus@web.de>
2021-12-12 18:41:31 +0100

    first try at parallel partition phase one

--- my_lib/include/library.h
+++ my_lib/include/library.h
@@ -1,6 +1,9 @@
 #ifndef PARALLEL_PARTITION_LIBRARY_H
 #define PARALLEL_PARTITION_LIBRARY_H
 
+#include <vector>
+#include <omp.h>
+
 namespace p_partition  {
     const int BOTH = 0;
     const int LEFT = 1;
@@ -37,6 +40,90 @@ namespace p_partition  {
             return LEFT;
         }
         return RIGHT;
+    }
+
+    const int BLOCK_SIZE = 2;
+
+    template <typename It1, typename It2>
+    bool block_available(It1 target, It2 counterpart) {
+        return std::distance(target, counterpart) >= BLOCK_SIZE-1;  // TODO: does this work? The reference says that negative distances are only possible for random access iterators (such as pointers)...
+    }
+
+    template <typename It>
+    It get_block(It *iter) {
+        It block = *iter;
+        *iter = std::next(*iter, BLOCK_SIZE);
+        return block;
+    }
+
+    // returns the remaining blocks
+    template <typename ForwardIt, typename BackwardIt, typename UnaryPredicate>
+    std::vector<ForwardIt> parallel_partition_phase(ForwardIt left, BackwardIt right, UnaryPredicate p, int *leftNeutralized, int *rightNeutralized) {
+        //auto pivot = *std::next(left, std::distance(left,right)/2); // naive pivot
+        std::vector<ForwardIt> remainingBlocks;
+        int ln, rn;
+#pragma omp parallel reduction(+: ln, rn)
+        {
+#pragma omp single
+            {
+                remainingBlocks = new std::vector<ForwardIt>(omp_get_num_threads());
+            }
+            ForwardIt leftBlock, rightBlock;
+            bool gotLeftBlock, gotRightBlock;
+            int leftCounter = 0, rightCounter = 0;
+#pragma omp critical
+            {   // get your first left block
+                gotLeftBlock = block_available(left, right);
+                if (gotLeftBlock) {
+                    leftBlock = get_block(&left);
+                }
+                // get your first right block
+                gotRightBlock = block_available(right, left);
+                if (gotRightBlock) {
+                    rightBlock = get_block(&right);
+                }
+            }
+            while (gotLeftBlock && gotRightBlock) {
+                int side = neutralize(leftBlock, rightBlock, p);
+#pragma omp critical
+                {
+                    // try to get new blocks, depending on which side was completed in the neutralize call
+                    switch (side) {
+                        case BOTH:
+                            ++leftCounter;
+                            gotLeftBlock = block_available(left, right);
+                            if (gotLeftBlock) {
+                                leftBlock = get_block(&left);
+                            }
+                            // don't break, just continue with the RIGHT case to get a right block as well
+                        case RIGHT:
+                            ++rightCounter;
+                            gotRightBlock = block_available(right, left);
+                            if (gotRightBlock) {
+                                rightBlock = get_block(&right);
+                            }
+                            break;
+                        case LEFT:
+                            ++leftCounter;
+                            gotLeftBlock = block_available(left, right);
+                            if (gotLeftBlock) {
+                                leftBlock = get_block(&left);
+                            }
+                            break;
+                    }
+                }
+            }
+#pragma omp critical
+            {
+                if (gotLeftBlock) remainingBlocks.push_back(leftBlock);
+                if (gotRightBlock) remainingBlocks.push_back(rightBlock);
+            }
+            ln += leftCounter * BLOCK_SIZE;
+            rn += rightCounter * BLOCK_SIZE;
+        }
+        *leftNeutralized = ln;
+        *rightNeutralized = ln;
+        return remainingBlocks;
     }
 }
 
