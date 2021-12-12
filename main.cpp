186e07f67715caefa9aa76b43a5dfd9aba3d9d88
PSteinhaus <philipp_steinhaus@web.de>
2021-12-12 15:41:19 +0100

    stopped indices from being wrongly reinitialized; introduced a library namespace;

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-    std::vector<float> v = {9,8,7,6,5,4,3,2,1,0};
+    std::vector<float> v = {4,8,7,6,5,4,3,2,1,0};
     int blocklength = 5;
     float pivot = 4.5;
 
     // test neutralize
-    int ret = neutralize(v.begin(),v.end()-5, blocklength, [pivot](const auto& em){ return em < pivot; });
+    int ret = p_partition::neutralize(v.begin(),v.begin()+5, blocklength, [pivot](const auto& em){ return em < pivot; });
 
     std::cout << "ret = " << ret << std::endl;
 
--- my_lib/include/library.h
+++ my_lib/include/library.h
@@ -1,41 +1,43 @@
 #ifndef PARALLEL_PARTITION_LIBRARY_H
 #define PARALLEL_PARTITION_LIBRARY_H
 
-#include <vector>
+namespace p_partition  {
+    const int BOTH = 0;
+    const int LEFT = 1;
+    const int RIGHT = 2;
 
-//returns 0 if Both neutralized, 1 if left ist neutralized, 2 if right is neutralized
-template <typename ForwardIt, typename UnaryPredicate>
-int neutralize(ForwardIt left, ForwardIt right, int blocksize, UnaryPredicate p){
-    int i , j ;
-    do {
-        for (i = 0; i < blocksize; i++){
-            left = std::next(left);
-            if (!p(*left))
+    //returns 0 if Both neutralized, 1 if left ist neutralized, 2 if right is neutralized
+    template <typename ForwardIt, typename UnaryPredicate>
+    int neutralize(ForwardIt left, ForwardIt right, int blocksize, UnaryPredicate p){
+        int i=0 , j=0;
+        do {
+            for (; i < blocksize; i++){
+                if (!p(*left))
+                    break;
+                left = std::next(left);
+            }
+            for (; j < blocksize; j++) {
+                if (p(*right))
+                    break;
+                right = std::next(right);
+            }
+            if ((i == blocksize) || (j == blocksize)){
                 break;
-        }
-        for (j = 0; j < blocksize; j++) {
-            right = std::next(right);
-            if (p(*right))
-                break;
-        }
-        if ((i == blocksize) || (j == blocksize)){
-            break;
-        }
+            }
 
-        std::swap(*left, *right);
-        i++;
-        j++;
-    } while ( i < blocksize && j < blocksize );
+            std::swap(*left, *right);
+            i++; left = std::next(left);
+            j++; right = std::next(right);
+        } while ( i < blocksize && j < blocksize );
 
-    std::cout << i << " " << j << std::endl;
-    if (i == blocksize) {
-        if (j == blocksize)
-            return 0;
-        return 1;
+        std::cout << i << " " << j << std::endl;
+        if (i == blocksize) {
+            if (j == blocksize)
+                return BOTH;
+            return LEFT;
+        }
+        return RIGHT;
     }
-    return 2;
 }
-
-
 
 #endif //PARALLEL_PARTITION_LIBRARY_H
